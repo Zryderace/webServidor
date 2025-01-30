98f2285f7200530d7e0c3af7a34e85a062f53cb9
zryderace <alemoled@gmail.com>
2025-01-30 17:43:54 +0100

    facugei2

--- API/nucleoAPI.php
+++ API/nucleoAPI.php
@@ -1,112 +1,148 @@
 <?php
 
-    error_reporting(E_ALL);
-    ini_set("display_errors",1);
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 
-    //se usa para enviar una cabecera HTTP a palo seco
-    //normalmente se redirige con location pero tambien se usa para indicar el tipo de dato contenido
-    header("Content-Type: application/json");
-    require "conexion_pdo.php";
+//se usa para enviar una cabecera HTTP a palo seco
+//normalmente se redirige con location pero tambien se usa para indicar el tipo de dato contenido
+header("Content-Type: application/json");
+require "conexion_pdo.php";
 
-    $metodo = $_SERVER["REQUEST_METHOD"];
-    //lee el cuerpo de la solicitud
-    $entrada = file_get_contents("php://input");
-    var_dump($entrada);
-    //de json a array clave
-    $entrada = json_decode($entrada,true);
+$metodo = $_SERVER["REQUEST_METHOD"];
+//lee el cuerpo de la solicitud
+$entrada = file_get_contents("php://input");
+var_dump($entrada);
+//de json a array clave
+$entrada = json_decode($entrada, true);
 
-    switch ($metodo) {
-        case 'GET':
-            controlGet($_conexion, $entrada);
-            break;
-        case 'POST':
-            controlPost($_conexion, $entrada);
-            break;
-        case 'PUT':
-            controlPut($_conexion, $entrada);
-            break;
-        case 'DELETE':
-            controlDelete($_conexion, $entrada);
-            break;
-        
-        default:
-            echo json_encode(["metodo" => "otro"]);
-            break;
+switch ($metodo) {
+    case 'GET':
+        controlGet($_conexion, $entrada);
+        break;
+    case 'POST':
+        controlPost($_conexion, $entrada);
+        break;
+    case 'PUT':
+        controlPut($_conexion, $entrada);
+        break;
+    case 'DELETE':
+        controlDelete($_conexion, $entrada);
+        break;
+
+    default:
+        echo json_encode(["metodo" => "otro"]);
+        break;
+}
+
+function controlGet($_conexion, $entrada)
+{
+    //sin ciudad, toda la tabla
+    //con ciudad, solo la linea que pertenezca
+    if (isset($_GET["ciudad"]) && $_GET["ciudad"] != "") {
+        $consulta = "SELECT * FROM desarrolladoras WHERE ciudad = :c";
+        $stmt = $_conexion->prepare($consulta);
+        $stmt->execute([
+            "c" => $_GET["ciudad"]
+        ]);
+    } else {
+        $consulta = "SELECT * FROM desarrolladoras";
+        $stmt = $_conexion->prepare($consulta);
+        $stmt->execute();
     }
 
-    function controlGet($_conexion, $entrada){
-        //sin ciudad, toda la tabla
-        //con ciudad, solo la linea que pertenezca
-        if (isset($_GET["ciudad"]) && $_GET["ciudad"] != "") {
-            $consulta = "SELECT * FROM desarrolladoras WHERE ciudad = :c";
-            $stmt = $_conexion -> prepare($consulta);
-            $stmt -> execute([
-                "c" => $_GET["ciudad"]
-            ]);
+    //devuelve todos los regstros en array asociativo
+    /**
+     * [
+     *  ["nombre_desarrolladora" => "Nintendo", "anno", "1995"],
+     *  ["nombre_desarrolladora" => "Valve", "anno", "2000"]
+     * ]
+     */
+    //solo para get
+    $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    //lo hacemos json otra vez
+    echo json_encode($res);
+}
+function controlPost($_conexion, $entrada)
+{
+    try {
+        $consulta = "INSERT INTO desarrolladoras (nombre_desarrolladora, ciudad, anno_fundacion) VALUES (:n, :c, :a)";
+        $stmt = $_conexion->prepare($consulta);
+        $stmt->execute([
+            "n" => $entrada["nombre_desarrolladora"],
+            "c" => $entrada["ciudad"],
+            "a" => $entrada["anno_fundacion"]
+        ]);
+        echo json_encode(["todo" => "bien"]);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        echo json_encode(["error" => "no se pudo meter"]);
+    }
+}
+function controlPut($_conexion, $entrada)
+{
+
+    //comprobar desarrolladora exista
+
+    $nombre_desarrolladora = isset($entrada["nombre_desarrolladora"]) && !empty($entrada["nombre_desarrolladora"]) ? $entrada["nombre_desarrolladora"] : "";
+
+    try {
+
+        $consulta = $_conexion->prepare("SELECT * FROM desarrolladoras WHERE nombre_desarrolladora = :nombre");
+
+        $consulta->execute(["nombre" => $nombre_desarrolladora]);
+
+        $fila = $consulta->fetch();
+
+        if ($fila) {
+            //aqui existe
+            try {
+                $consulta = "UPDATE desarrolladoras SET ciudad = :c, anno_fundacion = :a WHERE nombre_desarrolladora = :nombre";
+                $stmt = $_conexion->prepare($consulta);
+                $stmt->execute([
+                    "nombre" => $entrada["nombre_desarrolladora"],
+                    "c" => $entrada["ciudad"],
+                    "a" => $entrada["anno_fundacion"]
+                ]);
+                echo json_encode(["todo" => "bien"]);
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+                echo json_encode(["error" => "no se pudo update"]);
+            }
         } else {
-            $consulta = "SELECT * FROM desarrolladoras";
-            $stmt = $_conexion -> prepare($consulta);
-            $stmt -> execute();
+            echo json_encode(["error" => "no existe desarrolladora con ese nombre"]);
         }
-
-        //devuelve todos los regstros en array asociativo
-        /**
-         * [
-         *  ["nombre_desarrolladora" => "Nintendo", "anno", "1995"],
-         *  ["nombre_desarrolladora" => "Valve", "anno", "2000"]
-         * ]
-         */
-        //solo para get
-        $res = $stmt -> fetchAll(PDO::FETCH_ASSOC);
-
-        //lo hacemos json otra vez
-        echo json_encode($res);
+    } catch (PDOException $e) {
+        echo "ERROR: no se puede recuperar la consulta " . $e->getMessage();
     }
-    function controlPost($_conexion, $entrada){
-        try {
-            $consulta = "INSERT INTO desarrolladoras (nombre_desarrolladora, ciudad, anno_fundacion) VALUES (:n, :c, :a)";
-            $stmt = $_conexion -> prepare($consulta);
-            $stmt -> execute([
-            "n" => $entrada["nombre_desarrolladora"],
-            "c" => $entrada["ciudad"],
-            "a" => $entrada["anno_fundacion"]
-            ]);
-            echo json_encode(["todo" => "bien"]);
-            
-        } catch (PDOException $e) {
-            echo $e -> getMessage();
-            echo json_encode(["error" => "no se pudo meter"]);
+}
+function controlDelete($_conexion, $entrada)
+{
+    $nombre_desarrolladora = isset($entrada["nombre_desarrolladora"]) && !empty($entrada["nombre_desarrolladora"]) ? $entrada["nombre_desarrolladora"] : "";
+
+    try {
+        $consulta = $_conexion->prepare("SELECT * FROM desarrolladoras WHERE nombre_desarrolladora = :nombre");
+
+        $consulta->execute(["nombre" => $nombre_desarrolladora]);
+
+        $fila = $consulta->fetch();
+
+        if ($fila) {
+            try {
+                $consulta = "DELETE FROM desarrolladoras WHERE nombre_desarrolladora = :n";
+                $stmt = $_conexion->prepare($consulta);
+                $stmt->execute([
+                    "n" => $entrada["nombre_desarrolladora"],
+                ]);
+                echo json_encode(["todo" => "bien"]);
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+                echo json_encode(["error" => "no se pudo borrar"]);
+            }
+        } else {
+            echo json_encode(["error" => "no existe desarrolladora con ese nombre"]);
         }
+    } catch (PDOException $e) {
+        echo "ERROR: no se puede recuperar la consulta " . $e->getMessage();
     }
-    function controlPut($_conexion, $entrada){
-        try {
-            $consulta = "UPDATE desarrolladoras SET ciudad = :c, anno_fundacion = :a WHERE id_desarrolladora = :id";
-            $stmt = $_conexion -> prepare($consulta);
-            $stmt -> execute([
-            "id" => $entrada["id_desarrolladora"],
-            "c" => $entrada["ciudad"],
-            "a" => $entrada["anno_fundacion"]
-            ]);
-            echo json_encode(["todo" => "bien"]);
-        } catch (PDOException $e) {
-            echo $e -> getMessage();
-            echo json_encode(["error" => "no se pudo update"]);
-        }
-    }
-    function controlDelete($_conexion, $entrada){
-        try {
-            $consulta = "DELETE FROM desarrolladoras WHERE nombre_desarrolladora = :n";
-            $stmt = $_conexion -> prepare($consulta);
-            $stmt -> execute([
-            "n" => $entrada["nombre_desarrolladora"],
-            ]);
-            echo json_encode(["todo" => "bien"]);
-        } catch (PDOException $e) {
-            echo $e -> getMessage();
-            echo json_encode(["error" => "no se pudo borrar"]);
-        }
-    }
-
-
-
-?>
+}
--- API/prueba_api.php
+++ API/prueba_api.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
-    
+
     <div class="container m-4">
         <h1>test</h1>
         <form action="" method="post">
@@ -16,6 +18,8 @@
                 <select name="metodo" class="form-select">
                     <option value="GET">GET(Recuperar datos)</option>
                     <option value="POST">POST(Insertar datos)</option>
+                    <option value="PUT">PUT(Cambiar datos)</option>
+                    <option value="DELETE">DELETE(borrar datos)</option>
                 </select>
             </div>
             <div id="datosPost" class="mb-3">
@@ -29,52 +33,70 @@
     </div>
 
     <?php
-        if ($_SERVER["REQUEST_METHOD"]=="POST") {
-            $metodo = $_POST["metodo"];
-            $datos = [];
-            $url = "http://localhost/API/nucleoAPI.php";
-            if ($metodo=="GET") {
-                //mandamos un get, contruimos URL dependiendo de si da ciudad o no
-                $ciudad = isset($_POST["ciudad"]) && !empty($_POST["ciudad"]) ? "?ciudad=" . urlencode($_POST["ciudad"]) : "";
-                $url .= $ciudad;
-                // echo "url generada: " . htmlspecialchars($url) . "<br>";
-                // try {
-                //     $res = file_get_contents($url);
-                //     echo "<pre>" . htmlspecialchars($res) . "</pre>";
-                // } catch (Exception $e) {
-                //     echo "Error al realizar la solicitud " . $e->getMessage();
-                // }
-            } else if ($metodo=="POST") {
-                //mandamos un post, contruimos URL
-                $datos = [
-                    "nombre_desarrolladora" => $_POST["nombre_desarrolladora"],
-                    "ciudad" => $_POST["ciudad"],
-                    "anno_fundacion" => $_POST["anno_fundacion"]
-                ];
-            }
-            //controlar todo para que esten bien los datos
-            $opciones = [
-                "http" => [
-                    "header" => "Content-Type: application/jason",
-                    "method" => $metodo,
-                    "content" => json_encode($datos)
-                ]
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $metodo = $_POST["metodo"];
+        $datos = [];
+        $url = "http://localhost/API/nucleoAPI.php";
+        if ($metodo == "GET") {
+            //mandamos un get, contruimos URL dependiendo de si da ciudad o no
+            $ciudad = isset($_POST["ciudad"]) && !empty($_POST["ciudad"]) ? "?ciudad=" . urlencode($_POST["ciudad"]) : "";
+            $url .= $ciudad;
+            // echo "url generada: " . htmlspecialchars($url) . "<br>";
+            // try {
+            //     $res = file_get_contents($url);
+            //     echo "<pre>" . htmlspecialchars($res) . "</pre>";
+            // } catch (Exception $e) {
+            //     echo "Error al realizar la solicitud " . $e->getMessage();
+            // }
+        } else if ($metodo == "POST") {
+            //mandamos un post, contruimos URL
+            $datos = [
+                "nombre_desarrolladora" => $_POST["nombre_desarrolladora"],
+                "ciudad" => $_POST["ciudad"],
+                "anno_fundacion" => $_POST["anno_fundacion"]
             ];
+        } else if ($metodo == "PUT") {
+            //buscar que exista el nombre empresa
+            $nombre_desarrolladora = isset($_POST["nombre_desarrolladora"]) && !empty($_POST["nombre_desarrolladora"]) ? $_POST["nombre_desarrolladora"] : "";
 
-            //contexto PHP
-            $contexto = stream_context_create($opciones);
+            $datos = [
+                "nombre_desarrolladora" => $_POST["nombre_desarrolladora"],
+                "ciudad" => $_POST["ciudad"],
+                "anno_fundacion" => $_POST["anno_fundacion"]
+            ];
+        } elseif ($metodo === "DELETE") {
+            $nombre_desarrolladora = isset($_POST["nombre_desarrolladora"]) && !empty($_POST["nombre_desarrolladora"]) ? $_POST["nombre_desarrolladora"] : "";
 
-            try {
-                $respuesta = file_get_contents($url, false, $contexto);
-                //construye una conexion HTTP usando el contexto de stream context
-            } catch (Exception $e) {
-                echo "Error al realizar la solicitud " . $e->getMessage();
-            }
-
-            echo "<pre>" . htmlspecialchars($respuesta) . "</pre>";
+            $datos = [
+                "nombre_desarrolladora" => $_POST["nombre_desarrolladora"],
+                "ciudad" => $_POST["ciudad"],
+                "anno_fundacion" => $_POST["anno_fundacion"]
+            ];
         }
+        //controlar todo para que esten bien los datos
+        $opciones = [
+            "http" => [
+                "header" => "Content-Type: application/jason",
+                "method" => $metodo,
+                "content" => json_encode($datos)
+            ]
+        ];
+
+        //contexto PHP
+        $contexto = stream_context_create($opciones);
+
+        try {
+            $respuesta = file_get_contents($url, false, $contexto);
+            //construye una conexion HTTP usando el contexto de stream context
+        } catch (Exception $e) {
+            echo "Error al realizar la solicitud " . $e->getMessage();
+        }
+
+        echo "<pre>" . htmlspecialchars($respuesta) . "</pre>";
+    }
     ?>
 
 
 </body>
+
 </html>
